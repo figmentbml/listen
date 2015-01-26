8c934a868ea99e96554396edf0f38ee1a8cede1f
figmentbml <beth.jaswa@gmail.com>
2015-01-25 20:11:02 -0700

    updated player to stop instead of pause

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-$('#main').on('click', '.fa-play', function() {
-  $('audio').each(function() {this.pause()});
-  $('.fa-stop').addClass('fa-play').removeClass('fa-stop');
-  $(this).removeClass('fa-play').addClass('fa-stop');
-  $(this).parent().children('audio')[0].play();
-  var song = $(this).parent().children('span.title').text();
-  $('#select').text("Now playing: " + song);
+$(function (){
+  var playerEl = $("#player")[0];
+  var mainEl = $('#main');
+
+  mainEl.on('click', '.fa-play', function() {
+    $('.fa-stop').addClass('fa-play').removeClass('fa-stop');
+    $(this).removeClass('fa-play').addClass('fa-stop');
+    var file = $(this).parent().children('i').data('file');
+    playerEl.src = file;
+    playerEl.play();
+    var song = $(this).parent().children('span.title').text();
+    $('#select').text("Now playing: " + song);
+  }).on('click', '.fa-stop', function() {
+    $(this).removeClass('fa-stop').addClass('fa-play');
+    playerEl.pause();
+    playerEl.src = '';
+    $('#select').text("Select a song!");
+  });
+
+  $.getJSON('data.json', function(tracks) {
+    var $tracksTemplate = $('#tracksTemplate').html();
+
+    var newHTML = Mustache.to_html($tracksTemplate, tracks);
+
+    $('.tracks').html(newHTML);
+  });
 });
-
-$('#main').on('click', '.fa-stop', function() {
-  $(this).removeClass('fa-stop').addClass('fa-play');
-  $(this).parent().children('audio')[0].pause();
-  $('#select').text("Select a song!");
-});
-
-$.getJSON('data.json', function(tracks) {
-  var $tracksTemplate = $('#tracksTemplate').html();
-
-  var newHTML = Mustache.to_html($tracksTemplate, tracks);
-
-  $('.tracks').html(newHTML);
-})
